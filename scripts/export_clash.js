05e9a2eba0110d70c6b6e188de18ebb30e8d847f
exr-oss <vfc11mail@proton.me>
2026-01-02 18:37:30 +0300

    Update export_clash.js

--- scripts/export_clash.js
+++ scripts/export_clash.js
@@ -1,27 +1,17 @@
+// scripts/export_clash.js
 import fs from "fs";
 import YAML from "yaml";
 
-const nodes = JSON.parse(fs.readFileSync("work/nodes.final.json", "utf8"));
+const data = JSON.parse(fs.readFileSync("work/sticky.json"));
 
-const proxies = nodes.map((n, i) => ({
-  name: `NODE_${i}`,
-  type: "ss",
-  server: "0.0.0.0",
-  port: 443,
-  cipher: "aes-128-gcm",
-  password: "password"
-}));
-
-const cfg = {
-  port: 7890,
-  "socks-port": 7891,
-  mode: "rule",
-  proxies,
-  "proxy-groups": [
-    { name: "AUTO", type: "select", proxies: proxies.map(p => p.name) }
-  ],
-  rules: ["MATCH,AUTO"]
+const config = {
+  proxies: data.map(i => ({
+    name: i.id,
+    type: "http",
+    server: i.url,
+    port: 443
+  }))
 };
 
 fs.mkdirSync("export", { recursive: true });
-fs.writeFileSync("export/main.yaml", YAML.stringify(cfg));
+fs.writeFileSync("export/main.yaml", YAML.stringify(config));
